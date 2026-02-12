7d991327597651b414188be3d3d5daaad7b9e608
RangerW <74749916+Yongrui-Wang@users.noreply.github.com>
2026-02-12 12:38:58 +0800

    Add files via upload

---
+++ utils/loss.py
@@ -0,0 +1,191 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+def focal_loss(labels, logits, alpha, gamma):
+    """Compute the focal loss between `logits` and the ground truth `labels`.
+    Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
+    where pt is the probability of being classified to the true class.
+    pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
+    Args:
+      labels: A float tensor of size [batch, num_classes].
+      logits: A float tensor of size [batch, num_classes].
+      alpha: A float tensor of size [batch_size]
+        specifying per-example weight for balanced cross entropy.
+      gamma: A float scalar modulating loss from hard and easy examples.
+    Returns:
+      focal_loss: A float32 scalar representing normalized total loss.
+    """
+    BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
+
+    # if gamma == 0.0:
+    #     modulator = 1.0
+    # else:
+    #     modulator = torch.exp(-gamma * labels * logits - gamma * torch.log(1 +
+    #         torch.exp(-1.0 * logits)))
+    pt = torch.exp(-BCLoss)
+
+    # loss = modulator * BCLoss
+    F_loss = (1 - pt) ** gamma * BCLoss
+
+    weighted_loss = alpha * F_loss
+    focal_loss = torch.sum(weighted_loss)
+
+    focal_loss /= torch.sum(labels)
+    return focal_loss
+
+
+
+def CB_loss(labels, logits, samples_per_cls, no_of_classes, loss_type, beta, gamma):
+    """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes].
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+    effective_num = 1.0 - np.power(beta, samples_per_cls)
+    weights = (1.0 - beta) / np.array(effective_num)
+    weights = weights / np.sum(weights) * no_of_classes
+
+    labels_one_hot = F.one_hot(labels.squeeze().to(torch.int64), no_of_classes).float()
+
+    weights = torch.tensor(weights).float().to("cuda")
+    weights = weights.unsqueeze(0)
+    weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
+    weights = weights.sum(1)
+    weights = weights.unsqueeze(1)
+    weights = weights.repeat(1,no_of_classes)
+
+    if loss_type == "focal":
+        if len(labels_one_hot) != len(logits):
+            print("error")
+        cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
+    elif loss_type == "sigmoid":
+        cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
+    elif loss_type == "softmax":
+        pred = logits.softmax(dim = 1)
+        cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
+    return cb_loss
+
+
+
+# if __name__ == '__main__':
+    # no_of_classes = 2
+    # logits = torch.rand(5,no_of_classes).float()
+    # logits = torch.tensor([[0.2181, 0.8515],
+    #     [0.0102, 0.9692],
+    #     [0.3151, 0.7561],
+    #     [0.7949, 0.7821],
+    #     [0.6522, 0.7507]])
+    # labels = torch.randint(0,no_of_classes, size = (5,))
+    # labels = torch.tensor([1, 0, 0, 0, 0])
+    # beta = 0.999
+    # gamma = 2.0
+    # samples_per_cls = [1,10]
+    # loss_type = "focal"
+    # cb_loss = CB_loss(labels, logits, samples_per_cls, no_of_classes,loss_type, beta, gamma)
+    # print(cb_loss)
+
+
+class CenterLoss(nn.Module):
+    """Center loss.
+    Reference:
+    Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
+    Args:
+        num_classes (int): number of classes.
+        feat_dim (int): feature dimension.
+    """
+
+    def __init__(self, num_classes=2, feat_dim=2048, use_gpu=True):
+        super(CenterLoss, self).__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.use_gpu = use_gpu
+
+        if self.use_gpu:
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda())
+        else:
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))
+
+    def forward(self, x, labels):
+        """
+        Args:
+            x: feature matrix with shape (batch_size, feat_dim).
+            labels: ground truth labels with shape (num_classes).
+        """
+        assert x.size(0) == labels.size(0), "features.size(0) is not equal to labels.size(0)"
+
+        batch_size = x.size(0)
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+        distmat.addmm_(1, -2, x, self.centers.t())
+
+        classes = torch.arange(self.num_classes).long()
+        if self.use_gpu: classes = classes.cuda()
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+        print(mask)
+
+        dist = []
+        for i in range(batch_size):
+            print(mask[i])
+            value = distmat[i][mask[i]]
+            value = value.clamp(min=1e-12, max=1e+12)  # for numerical stability
+            dist.append(value)
+        dist = torch.cat(dist)
+        loss = dist.mean()
+        return loss
+
+
+class TripletCenterLoss(nn.Module):
+    def __init__(self, margin=5, num_classes=2, center_embed=2):
+        super(TripletCenterLoss, self).__init__()
+        self.margin = margin
+        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        self.centers = nn.Parameter(torch.randn(num_classes, center_embed)).to("cuda")
+
+    def forward(self, inputs, targets):
+        targets = targets.to(torch.int64)
+        batch_size = inputs.size(0)
+        targets_expand = targets.view(batch_size, 1).expand(batch_size, inputs.size(1))
+        centers_batch = self.centers.gather(0, targets_expand)  # centers batch
+
+        # compute pairwise distances between input features and corresponding centers
+        centers_batch_bz = torch.stack([centers_batch] * batch_size)
+        inputs_bz = torch.stack([inputs] * batch_size).transpose(0, 1)
+        dist = torch.sum((centers_batch_bz - inputs_bz) ** 2, 2).squeeze()
+        dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+
+        # for each anchor, find the hardest positive and negative
+        mask = targets.expand(batch_size, batch_size).eq(targets.expand(batch_size, batch_size).t())
+        dist_ap, dist_an = [], []
+        for i in range(batch_size):  # for each sample, we compute distance
+            dist_ap.append(dist[i][mask[i]].max())  # mask[i]: positive samples of sample i
+            dist_an.append(dist[i][mask[i] == 0].min())  # mask[i]==0: negative samples of sample i
+
+        # dist_ap = torch.cat(dist_ap)
+        # dist_an = torch.cat(dist_an)
+        dist_ap = torch.tensor(dist_ap).squeeze()
+        dist_an = torch.tensor(dist_an).squeeze()
+        # generate a new label y
+        # compute ranking hinge loss
+        y = dist_an.data.new()
+        y.resize_as_(dist_an.data)
+        y.fill_(1)
+        # y_i = 1, means dist_an > dist_ap + margin will casuse loss be zero
+        loss = self.ranking_loss(dist_an, dist_ap, y)
+
+        # prec = (dist_an.data > dist_ap.data).sum() * 1. / y.size(0)  # normalize data by batch size
+        return loss
+
+# if __name__ == '__main__':
